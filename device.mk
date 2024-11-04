433ae29d3451e440c69a853b4852a3336233530b
Ghosuto <clash.raja10@gmail.com>
2024-11-04 14:11:35 +0000

    Pong: Build libstagefright_foundation-v33 package

--- device.mk
+++ device.mk
@@ -206,8 +206,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
+PRODUCT_PACKAGES += \
+    libstagefright_foundation-v33
 
 # DRM
 PRODUCT_PACKAGES += \
