ff975f861d45980bc8c73e01a321a5e5a3682ba4
darknecromonger <dark.necro@yahoo.com>
2024-12-15 19:30:46 +0530

    Setup deviceaswebcam

--- device.mk
+++ device.mk
@@ -167,6 +167,10 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     Pong_DeviceExtras
 
+#DeviceasWebcam
+PRODUCT_PACKAGES += \
+    PongDeviceAsWebcam
+
 # Display
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/displayconfig/display_id_4630946639017191810.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946639017191810.xml
