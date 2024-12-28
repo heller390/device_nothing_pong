9fa8dc785b8bb678473f176db07fbe138de632bc
heller390 <heller390@hotmail.com>
2024-12-28 13:11:19 +0530

    Add vos specific redundant packages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,27 +5,13 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-    Chrome \
-    Chrome-Stub \
-    Drive \
-    GoogleCamera \
-    Maps \
-    MyVerizonServices \
-    OBDM_Permissions \
-    obdm_stub \
-    OemDmTrigger \
-    OPScreenRecord \
-    Ornament \
-    SafetyHubPrebuilt \
-    ScribePrebuilt \
-    Showcase \
-    Snap \
-    SprintDM \
-    SprintHM \
-    VZWAPNLib \
-    VzwOmaTrigger \
-    YouTube \
-    YouTubeMusicPrebuilt
+    Etar \
+    Jellyfish \
+    Gramophone \
+    Seedvault \
+    ExactCalculator \
+    QuickAccessWallet \
+    Stk
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
